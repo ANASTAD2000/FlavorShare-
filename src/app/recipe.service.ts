20253a1f818de29edbc930746265785cbf4b9250
ANASTAD2000 <anasbaclibr@gmai.com>
2025-04-30 12:32:24 +0100

    add RecipeService to fetch recipes from external API

---
+++ src/app/recipe.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'
+
+  constructor(private http : HttpClient) { }
+  getRecipes() : Observable<any>{
+    return this.http.get<any>(this.apiUrl)
+  }
+}
